0606bba9a8e90c9af3cf78669be64596627bbbae
codeychoi <codeychoi@gmail.com>
2025-05-04 17:40:14 +0900

    [Bronze V] Title: A+B - 2, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    Scanner sc = new Scanner(System.in);
+    byte a = sc.nextByte();
+    byte b = sc.nextByte();
+
+    System.out.println(a+b);
+
+    }
+}
