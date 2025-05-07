b9507dca55bd783ddb2005fa09cfdcf73c3578ac
codeychoi <codeychoi@gmail.com>
2025-05-07 20:24:14 +0900

    [Bronze IV] Title: 기찍 N, Time: 764 ms, Memory: 31404 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
