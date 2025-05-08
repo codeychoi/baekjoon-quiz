e4ac184439c5daf118ac45b5a7646a8e948bf449
codeychoi <codeychoi@gmail.com>
2025-05-08 20:23:59 +0900

    [Bronze IV] Title: AFC 윔블던, Time: 124 ms, Memory: 16040 KB -BaekjoonHub

--- 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
+++ 백준/Bronze/4299. AFC 윔블던/AFC 윔블던.java
@@ -5,30 +5,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         int sum = Integer.parseInt(st.nextToken());
         int sub = Integer.parseInt(st.nextToken());
 
-        // 조건 1, 2 검사
-        if (sum < sub || (sum + sub) % 2 != 0) {
-            System.out.println(-1);
-            return;
-        }
+        //1. sum >= sub 여야함 0:0일수도 있으니까
+        //2. (sum+sub)%2 !=0 면 -1임
+        //3. team1> team2 비교해야함 team1과 team2는 음이 아닌 정수
 
-        int team1 = (sum + sub) / 2;
+        int team1 = (sum+sub)/2;
         int team2 = sum - team1;
 
-        // 조건 3 검사
-        if (team1 < 0 || team2 < 0) {
+        if(sum<sub || (sum+sub)%2 !=0 || sum < 0 || sub < 0 || team1 <0 || team2< 0){
             System.out.println(-1);
             return;
+        }else{
+            if(team1>=team2){
+                System.out.println(team1+ " " +team2);
+            }else{
+                System.out.println(team2+ " " +team1);
+            }
         }
-
-        // 큰 점수 먼저 출력
-        if (team1 > team2) {
-            System.out.println(team1 + " " + team2);
-        } else {
-            System.out.println(team2 + " " + team1);
-        }
+        
     }
 }
